a0cc072526d1541edf2604d7dfd66a60e1079529
PraSaw16 <pratibhasawant@gmail.com>
2023-08-30 13:11:34 +0200

    First commit JS prog

---
+++ 01-basic.js
@@ -0,0 +1,18 @@
+
+console.log("Pratibha");
+
+const accountID = 67566;
+let accountEmail = "prat@gmail.com";
+var accountPassword ="jjjj";
+let accountState;
+let accountBill=45666;
+
+/*
+Prefer not to use var because of issue in block scope and functional scope
+*/
+
+// const and let can use
+// when you define variable without value then type will be undefined.
+
+console.log(accountID);
+console.table([accountID,accountEmail,accountPassword,accountState,accountBill]);
---
+++ 02-dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; // use newer version of javascript
+
+//JS engine is hidden in browser.
+
+
+// alert(3+4)     // we are using nodejs,not browseer.
+
+//react and node -dont use semicolon
+//code should be readeable. Use semicolon and space at right place.
+
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript
+
+//https://tc39.es/
+
+/*
+Number
+bigint
+string
+boolean
+null- standalone value
+undefined- value is not defined yet
+symbol- to define uniqueness
+
+*/
+
+let score= null;
+let isLoggedIn= ""
+console.log(typeof undefined );
+
+console.log(typeof null );
